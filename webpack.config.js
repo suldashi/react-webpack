d698479a001e0312e0996bc9cf5c2f88dbd0e040
Ermir Suldashi <ermirsuldashi@gmail.com>
2019-11-27 16:17:19 +0100

    added scss loader and fixed up the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development'
+  return {
     entry: './src/ui/ui.jsx',
     module: {
       rules: [
@@ -6,22 +10,57 @@ module.exports = {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: ['babel-loader']
+        },
+        {
+          test: /\.module\.s(a|c)ss$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          exclude: /\.module\.s(a|c)ss$/,
+          loader: [
+            isDevelopment? 'style-loader': MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
         }
       ]
     },
     resolve: {
-      extensions: ['*', '.js', '.jsx']
+      extensions: ['*', '.js', '.jsx', '.scss']
     },
     output: {
-      path: __dirname + '/public/js',
+      path: __dirname + '/public',
       publicPath: '/',
-      filename: 'ui.js'
+      filename: 'js/ui.js'
     },
-    devServer: {
-      contentBase: './public/js'
-    },
-    watch: true,
     watchOptions: {
       ignored: ["node_modules","app","public"]
-    }
-  };
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: isDevelopment? 'css/style.css' : 'css/style.[hash].css',
+        chunkFilename: isDevelopment ? 'css/[id].css' : 'css/[id].[hash].css'
+      })
+    ]
+  }};
